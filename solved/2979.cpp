b111e9cf49cc3fdc79d58293311c2b6e38816653
jeonghwan <jxxnghwxn@gmail.com>
2025-03-01 22:54:22 +0900

    2979 model answer

--- solved/2979.cpp
+++ solved/2979.cpp
@@ -1,9 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b, c, cost;
-int minT = 100, maxT = 1, cnt;
-int ti[3][2];
+// int a, b, c, cost;
+// int cnt;
+// int ti[3][2];
+
+int a, b, c, ret;
+int cnt[100];
+int inTime, outTime;
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -12,34 +16,54 @@ int main() {
 
   cin >> a >> b >> c;
   for (int i = 0; i < 3; ++i) {
-    for (int j = 0; j < 2; ++j) {
-      cin >> ti[i][j];
-      minT = min(minT, ti[i][j]);
-      maxT = max(maxT, ti[i][j]);
+    cin >> inTime >> outTime;
+    for (int j = inTime; j < outTime; ++j) {
+      cnt[j]++;
     }
   }
 
-  for (int i = minT; i < maxT; ++i) {
-    for (int j = 0; j < 3; ++j) {
-      if (i >= ti[j][0] && i < ti[j][1]) {
-        cnt++;
+  for (int j = 1; j < 100; ++j) {
+    if (cnt[j]) {
+      if (cnt[j] == 1) {
+        ret += a;
+      } else if (cnt[j] == 2) {
+        ret += b * 2;
+      } else if (cnt[j] == 3) {
+        ret += c * 3;
       }
     }
-    switch (cnt) {
-      case 1:
-        cost += a * 1;
-        break;
-      case 2:
-        cost += b * 2;
-        break;
-      case 3:
-        cost += c * 3;
-        break;
-      default:
-        break;
-    }
-    cnt = 0;
   }
-  cout << cost;
+
+  cout << ret;
+  // cin >> a >> b >> c;
+  // for (int i = 0; i < 3; ++i) {
+  //   for (int j = 0; j < 2; ++j) {
+  //     cin >> ti[i][j];
+  //
+  //   }
+  // }
+
+  // for (int i = 1; i < 100; ++i) {
+  //   for (int j = 0; j < 3; ++j) {
+  //     if (i >= ti[j][0] && i < ti[j][1]) {
+  //       cnt++;
+  //     }
+  //   }
+  //   switch (cnt) {
+  //     case 1:
+  //       cost += a * 1;
+  //       break;
+  //     case 2:
+  //       cost += b * 2;
+  //       break;
+  //     case 3:
+  //       cost += c * 3;
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  //   cnt = 0;
+  // }
+  // cout << cost;
   return 0;
 }
